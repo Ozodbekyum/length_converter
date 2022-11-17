311c17529e19932f8a07a99cadfb885da2f320a0
ZARIFMAC <uzbzarif@gmail.com>
2022-11-17 16:09:54 +0500

    convert cm to m

--- bin/length_converter.dart
+++ bin/length_converter.dart
@@ -1,6 +1,6 @@
 import 'package:length_converter/length_converter.dart';
 
 void main() {
-  Length l1 = Length(10, 'm');
-  print(l1.unit);
+  Length l1 = Length(100, 'cm');
+  print(l1.valueIn('m'));
 }
--- lib/length_converter.dart
+++ lib/length_converter.dart
@@ -6,4 +6,24 @@ class Length {
     this.x = x;
     this.unit = unit;
   }
+
+  double valueIn(String unit) {
+    if (this.unit == 'm') {
+      if (unit == 'm') {
+        return this.x;
+      }
+
+      if (unit == 'cm') {
+        return this.x * 100;
+      }
+    }
+
+    if (this.unit == 'cm') {
+      if (unit == 'm') {
+        return this.x / 100;
+      }
+    }
+
+    return 0;
+  }
 }
